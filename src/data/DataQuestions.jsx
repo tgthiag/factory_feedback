c4913505808c0f19638586148d5614dd18a58556
tgthiag <tgthiag@gmail.com>
2023-06-12 09:21:01 -0300

    Data class ready

---
+++ src/data/DataQuestions.jsx
@@ -0,0 +1,12 @@
+class DataQuestions {
+    constructor(value1, value2, value3, value4, value5, stringValue) {
+      this.value1 = value1;
+      this.value2 = value2;
+      this.value3 = value3;
+      this.value4 = value4;
+      this.value5 = value5;
+      this.stringValue = stringValue;
+    }
+  }
+  
+  export default DataQuestions;
