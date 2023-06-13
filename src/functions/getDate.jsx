e35734f6382e9a97a7917bf2c0cc8b6407531320
tgthiag <tgthiag@gmail.com>
2023-06-13 08:17:55 -0300

    Date functions

--- src/functions/getDate.jsx
+++ src/functions/getDate.jsx
@@ -1,4 +1,4 @@
-export default function getDate() {
+export function getDate() {
     const date = new Date();
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -8,5 +8,30 @@ export default function getDate() {
 
     const formattedDate = `${year}-${month}-${day}-${hour}-${minute}`;
     console.log(formattedDate);
+    return formattedDate;
+}
+
+export function yearMonth() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1);
+
+    const formattedDate = `${year}-${month}`;
+    return formattedDate;
+}
+
+export function getYear() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    const formattedDate = `${year}`;
+    return formattedDate;
+}
+
+export function getMonth() {
+    const date = new Date();
+    const month = String(date.getMonth() + 1);
+
+    const formattedDate = `${month}`;
     return formattedDate;
 }
