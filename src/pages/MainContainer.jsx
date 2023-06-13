619b684ff41784b8969ab429bac9410a0c7e4069
tgthiag <tgthiag@gmail.com>
2023-06-13 08:17:01 -0300

    Thanks page

--- src/pages/MainContainer.jsx
+++ src/pages/MainContainer.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyHeader from '../components/Header';
 import BodySelectConfig from './BodySelectConfig';
 import HomePage from './HomePage';
+import CongratsPage from './congratsPage';
 
 function MainContainer() {
   return (
@@ -12,6 +13,7 @@ function MainContainer() {
         <Routes>
           <Route path="/" element={<BodySelectConfig/>} />
           <Route path="/home_page" element={<HomePage/>} />
+          <Route path="/thanks" element={<CongratsPage/>} />
         </Routes>
         </BrowserRouter>
     </div>
---
+++ src/pages/congratsPage.jsx
@@ -0,0 +1,12 @@
+import Title from "../components/title";
+
+function CongratsPage(){
+    return(
+        <div>
+            <Title string={"Agradecemos!"}/>
+            <p style={{ color: "black", fontWeight: "bold", fontSize: 18 }}>Sua opnião nos ajuda a evoluir cada vez mais!</p>
+        </div>
+    )
+};
+
+export default CongratsPage;
